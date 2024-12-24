6564cfd50e63809f6f8a1a143d5f0100a2ad0a6e
yahia <yahiamhammad@gmail.com>
2024-12-25 01:23:19 +0200

    initial commit

---
+++ LinkedList.h
@@ -0,0 +1,330 @@
+#ifndef LINKEDLIST
+#define LINKEDLIST
+
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+template<typename ElementType>
+class LinkedList {
+
+	private:
+		//Creating the class node in this list
+		//like that it is only being accessed by private attributr of linked list
+		class Node {
+			public:
+			ElementType data;
+			Node* next;
+
+			Node()
+			:next(0)
+			{}
+
+			Node(ElementType value)
+			:next(0), data(value)
+			{}
+
+
+		};
+		int size;
+
+	public:
+		typedef Node* NodePointer;
+	private:
+		NodePointer first;
+	public:
+		LinkedList() :size(0), first(0)
+		{}
+
+		LinkedList(const LinkedList& original) : first(0), size(original.size)
+		{
+			if (size == 0) return;
+
+			NodePointer OrigPtr, LastPtr;
+			first = new Node(original.first->data);
+			LastPtr = first;
+			OrigPtr = original.first->next;
+
+			while (OrigPtr != 0) {
+				LastPtr->next = new Node(OrigPtr->data);
+				LastPtr = LastPtr->next;
+				OrigPtr = OrigPtr->next;
+			}
+		}
+
+		~LinkedList(){
+			LinkedList::NodePointer prev = first, pointer;
+
+				while (prev != 0) {
+					pointer = prev->next;
+					delete prev;
+					prev = pointer;
+				}
+		}
+
+		const LinkedList & operator=(const LinkedList& righthandside){
+			{	size = righthandside.size;
+				first = 0;
+				if (size == 0)return *this;
+
+				if (this != &righthandside) {
+					this->~LinkedList();
+					LinkedList::NodePointer OrigPtr, LastPtr;
+					first = new Node(righthandside.first->data);
+					LastPtr = first;
+					OrigPtr = righthandside.first->next;
+
+					while (OrigPtr != 0) {
+						LastPtr->next = new Node(OrigPtr->data);
+						OrigPtr = OrigPtr->next;
+						LastPtr = LastPtr->next;
+					}
+				}
+				return *this;
+			}
+		}
+
+		bool empty() const{
+				return first == 0;
+		}
+
+		void insert(ElementType dataval) {
+		    NodePointer newptr = new Node(dataval);
+		    newptr->next = nullptr;
+
+		    if (size == 0) {
+		        first = newptr;
+		    } else {
+		        NodePointer predptr = first;
+		        for (int i = 0; i < size - 1; i++) {
+		            predptr = predptr->next;
+		        }
+		        predptr->next = newptr;
+		    }
+		    size++;
+		}
+
+	    void insertInFront(ElementType dataval) {
+			NodePointer newptr = new Node(dataval);
+			newptr->next = first;
+			first = newptr;
+			size++;
+		}
+
+		void erase(int pos){
+			{
+				if (pos < 0 || pos >= size) {
+					cerr << "This position you want to delete/erase is invalid \n";
+					return;
+				}
+				size--;
+
+				LinkedList::NodePointer ptr, Predptr = first;
+				if (pos == 0) {
+
+					ptr = first;
+					first = Predptr->next;
+					delete ptr;
+
+				}
+				else {
+					for (int i = 1;i < pos;i++)
+						Predptr = Predptr->next;
+					ptr = Predptr->next;
+					Predptr->next = ptr->next;
+					delete ptr;
+				}
+
+			}
+		}
+
+	    void eraseLast() {
+			NodePointer ptr = first;
+			if (size == 0) {
+				cerr << "This position you want to delete/erase is invalid \n";
+			}
+			else if (size == 1)
+			{
+				delete first;
+				first = nullptr;
+			}
+			else
+			{
+				while (ptr->next->next != nullptr)
+					ptr = ptr->next;
+				delete ptr->next;
+				ptr->next = nullptr;
+			}
+			size--;
+		}
+
+		void display(ostream& out) const {
+			LinkedList::NodePointer ptr = first;
+
+			while (ptr != 0) {
+				cout << ptr->data << " ";
+				ptr = ptr->next;
+			}
+		}
+
+		/*Note: the node count is not a static variable because if it were it would be
+		stored within the node class and not the list, because different lists can have a different
+		node count that is not sa7*/
+		int nodecount()const{
+
+				if (this->empty())
+					return 0;
+				NodePointer ptr = first;
+
+				int count = 1;
+
+				while (ptr->next != 0) {
+					ptr = ptr->next;
+					count++;
+
+				}
+
+				return count;
+		}
+
+		void clear() {
+			NodePointer pred = first, ptr = first->next;
+			while (ptr != nullptr) {
+				pred = ptr;
+				ptr = ptr->next;
+				delete pred;
+			}
+			first = 0;
+			size = 0;
+		}
+
+
+		void insertbeforelast(ElementType x) {
+
+				NodePointer ptr = first, newptr = new Node(x);
+
+				while (ptr->next != 0) {
+					ptr = ptr->next;
+				}
+				newptr->next = ptr->next;
+				ptr->next = newptr;
+
+		}
+
+
+
+
+
+		bool Search(ElementType x) const
+		{
+
+				NodePointer pred = first;
+
+				if (pred->data == x) {
+					return true;
+				}
+				while (pred->next != 0) {
+					if (pred->data == x) {
+						return true;
+					}
+					pred = pred->next;
+				}
+
+				return false;
+
+		}
+
+		void inseraftern(int n, ElementType dataval)
+		{
+
+
+				NodePointer ptr = first, newptr = new Node(dataval);
+				//the n is 0 based stating from the first node
+
+				// if 0 it inserts after the 0t =h node which is the first node
+				if (n == 0) {
+					newptr->next = first->next;
+					first->next = newptr;
+				}
+				else {
+
+					for (int i = 0;i < n;i++) {
+						ptr = ptr->next;
+					}
+					newptr->next = ptr->next;
+					ptr->next = newptr;
+				}
+
+		}
+
+		void deletenth(int n){
+				NodePointer ptr = first, pred = first;
+
+				if (n == 0) {
+					first = ptr->next;
+					delete ptr;
+				}
+				else {
+					for (int i = 1;i < n;i++) {
+						pred = pred->next;
+					}
+					ptr = pred->next;
+					pred->next = ptr->next;
+					delete ptr;
+				}
+		}
+
+		LinkedList merge(LinkedList & l1, LinkedList & l2){
+			{
+
+				NodePointer l1ptr = l1.first, l2ptr = l2.first;
+
+
+				LinkedList myfinal;
+
+
+				int index = 0;
+				while (l1ptr != 0 && l2ptr != 0) {
+
+					if ((l1ptr->data) < (l2ptr->data)) {
+						myfinal.insert(l1ptr->data, index);
+						index++;
+						l1ptr = l1ptr->next;
+					}
+					else {
+						myfinal.insert(l2ptr->data, index);
+						index++;
+						l2ptr = l2ptr->next;
+					}
+
+				}
+
+				while (l1ptr != 0) {
+					myfinal.insert(l1ptr->data, index);
+					index++;
+					l1ptr = l1ptr->next;
+				}
+
+				while (l2ptr != 0) {
+					myfinal.insert(l2ptr->data, index);
+					l2ptr = l2ptr->next;
+					index++;
+				}
+
+
+				return myfinal;
+			}
+		}
+
+		NodePointer getHead() {
+			return first;
+		}
+
+
+};
+
+
+
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,259 @@
+#include <iostream>
+#include <raylib.h>
+#include <raymath.h>
+#include "LinkedList.h"
+using namespace std;
+
+static bool allowMove = false;
+Color green = {173, 204, 96, 255};
+Color darkGreen = {43, 51, 24, 255};
+
+int cellSize = 30;
+int cellCount = 25;
+int offset = 75;
+
+double lastUpdateTime = 0;
+
+
+bool EventTriggered(double interval)
+{
+    double currentTime = GetTime();
+    if (currentTime - lastUpdateTime >= interval)
+    {
+        lastUpdateTime = currentTime;
+        return true;
+    }
+    return false;
+}
+
+class Snake
+{
+public:
+    LinkedList<Vector2> body ;//= {Vector2{6, 9}, Vector2{5, 9}, Vector2{4, 9}};
+    Vector2 direction = {1, 0};
+    bool addSegment = false;
+    Snake() {
+        body.insert(Vector2{6, 9});
+        body.insert(Vector2{5, 9});
+        body.insert(Vector2{4, 9});
+    }
+
+    void Draw()
+    {
+        LinkedList<Vector2>::NodePointer ptr = body.getHead();
+        while (ptr != nullptr)
+        {
+            float x = ptr->data.x;
+            float y = ptr->data.y;
+            Rectangle segment = Rectangle{offset + x * cellSize, offset + y * cellSize, (float)cellSize, (float)cellSize};
+            DrawRectangleRounded(segment, 0.5, 6, darkGreen);
+            ptr = ptr->next;
+        }
+    }
+
+    void Update()
+    {
+        body.insertInFront(Vector2Add(body.getHead()->data, direction));
+        if (addSegment == true)
+        {
+            addSegment = false;
+        }
+        else
+        {
+
+            body.eraseLast();
+        }
+    }
+
+    void Reset()
+    {
+        body.clear();
+        body.insert(Vector2{6, 9});
+        body.insert(Vector2{5, 9});
+        body.insert(Vector2{4, 9});
+        direction = {1, 0};
+    }
+};
+
+class Food
+{
+
+public:
+    Vector2 position;
+    Texture2D texture;
+
+    Food(const LinkedList<Vector2> &snakeBody)
+    {
+        Image image = LoadImage("Graphics/food.png");
+        texture = LoadTextureFromImage(image);
+        UnloadImage(image);
+        position = GenerateRandomPos(snakeBody);
+    }
+
+    ~Food()
+    {
+        UnloadTexture(texture);
+    }
+
+    void Draw()
+    {
+        DrawTexture(texture, offset + position.x * cellSize, offset + position.y * cellSize, WHITE);
+    }
+
+    Vector2 GenerateRandomCell()
+    {
+        float x = GetRandomValue(0, cellCount - 1);
+        float y = GetRandomValue(0, cellCount - 1);
+        return Vector2{x, y};
+    }
+
+    Vector2 GenerateRandomPos(const LinkedList<Vector2>& snakeBody)
+    {
+        Vector2 position = GenerateRandomCell();
+        while (snakeBody.Search(position))
+        {
+            position = GenerateRandomCell();
+        }
+        return position;
+    }
+};
+
+class Game
+{
+public:
+    Snake snake = Snake();
+    Food food = Food(snake.body);
+    bool running = true;
+    int score = 0;
+    Sound eatSound;
+    Sound wallSound;
+
+    Game()
+    {
+        InitAudioDevice();
+        eatSound = LoadSound("Sounds/eat.mp3");
+        wallSound = LoadSound("Sounds/wall.mp3");
+    }
+
+    ~Game()
+    {
+        UnloadSound(eatSound);
+        UnloadSound(wallSound);
+        CloseAudioDevice();
+    }
+
+    void Draw()
+    {
+        food.Draw();
+        snake.Draw();
+    }
+
+    void Update()
+    {
+        if (running)
+        {
+            snake.Update();
+            CheckCollisionWithFood();
+            CheckCollisionWithEdges();
+            CheckCollisionWithTail();
+        }
+    }
+
+    void CheckCollisionWithFood()
+    {
+        if (Vector2Equals(snake.body.getHead()->data, food.position))
+        {
+            food.position = food.GenerateRandomPos(snake.body);
+            snake.addSegment = true;
+            score++;
+            PlaySound(eatSound);
+        }
+    }
+
+    void CheckCollisionWithEdges()
+    {
+        if (snake.body.getHead()->data.x == cellCount || snake.body.getHead()->data.x == -1)
+        {
+            GameOver();
+        }
+        if (snake.body.getHead()->data.y == cellCount || snake.body.getHead()->data.y == -1)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        snake.Reset();
+        food.position = food.GenerateRandomPos(snake.body);
+        running = false;
+        score = 0;
+        PlaySound(wallSound);
+    }
+
+    void CheckCollisionWithTail()
+    {
+        LinkedList<Vector2> headlessBody = snake.body;
+        headlessBody.erase(0);
+        if (headlessBody.Search(snake.body.getHead()->data) == true)
+        {
+            GameOver();
+        }
+    }
+};
+
+int main()
+{
+    cout << "Starting the game..." << endl;
+    InitWindow(2 * offset + cellSize * cellCount, 2 * offset + cellSize * cellCount, "Retro Snake");
+    SetTargetFPS(60);
+
+    Game game = Game();
+
+    while (WindowShouldClose() == false)
+    {
+        BeginDrawing();
+
+        if (EventTriggered(0.2))
+        {
+            allowMove = true;
+            game.Update();
+        }
+
+        if (IsKeyPressed(KEY_UP) && game.snake.direction.y != 1 && allowMove)
+        {
+            game.snake.direction = {0, -1};
+            game.running = true;
+            allowMove = false;
+        }
+        if (IsKeyPressed(KEY_DOWN) && game.snake.direction.y != -1 && allowMove)
+        {
+            game.snake.direction = {0, 1};
+            game.running = true;
+            allowMove = false;
+        }
+        if (IsKeyPressed(KEY_LEFT) && game.snake.direction.x != 1 && allowMove)
+        {
+            game.snake.direction = {-1, 0};
+            game.running = true;
+            allowMove = false;
+        }
+        if (IsKeyPressed(KEY_RIGHT) && game.snake.direction.x != -1 && allowMove)
+        {
+            game.snake.direction = {1, 0};
+            game.running = true;
+            allowMove = false;
+        }
+
+        // Drawing
+        ClearBackground(green);
+        DrawRectangleLinesEx(Rectangle{(float)offset - 5, (float)offset - 5, (float)cellSize * cellCount + 10, (float)cellSize * cellCount + 10}, 5, darkGreen);
+        DrawText("Snake Game", offset - 5, 20, 40, darkGreen);
+        DrawText(TextFormat("%i", game.score), offset - 5, offset + cellSize * cellCount + 10, 40, darkGreen);
+        game.Draw();
+
+        EndDrawing();
+    }
+    CloseWindow();
+    return 0;
+}
